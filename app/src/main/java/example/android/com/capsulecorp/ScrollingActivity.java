276b8839dcb2d6d40b2d9abb541f32a51d027022
Kentux <tiagogomes1209@gmail.com>
2017-03-28 15:22:49 +0100

    Added opening hours and intent to send email when clicking the floating button.

--- app/src/main/java/example/android/com/capsulecorp/ScrollingActivity.java
+++ app/src/main/java/example/android/com/capsulecorp/ScrollingActivity.java
@@ -21,15 +21,6 @@ public class ScrollingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -52,10 +43,10 @@ public class ScrollingActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-    public void emailButtonClick () {
+    public void emailButtonClick (View view) {
         Intent emailButton = new Intent(Intent.ACTION_SENDTO);
         emailButton.setData(Uri.parse("mailto:"));
-        emailButton.putExtra(Intent.EXTRA_EMAIL, "capsulecorp@capscorp.com");
+        emailButton.putExtra(Intent.EXTRA_EMAIL, new String [] {"capsulecorp@capscorp.com"});
         if (emailButton.resolveActivity(getPackageManager()) != null) {
             startActivity(emailButton);
         }
